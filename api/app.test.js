d2546cd89693f370ed1544d3eb6a3e80ad415304
nohaynicksdisponibles <jonathanezequielsosa@hotmail.com>
2021-06-19 09:45:53 -0300

    test finalizados

--- api/app.test.js
+++ api/app.test.js
@@ -4,7 +4,7 @@ const supertest = require('supertest')
 const request = supertest(app)
 //const {obtainOpenApi} = require("./controllers/controllers")
 
-describe('testing routes', () => {
+describe('testing routes with private ip', () => {
   it('Gets the open api documentation about the app', () => {
     jest.setTimeout(request.get('/v1').then(data => {
       const ob = JSON.parse(data.text)
@@ -12,10 +12,46 @@ describe('testing routes', () => {
     }),5000)
   })
 
-  it('Gets the an object with information about the location', () => {
+  it('Gets the object with information about the location', () => {
     jest.setTimeout(request.get('/location').then(data => {
       const ob = JSON.parse(data.text)
       expect(ob.location.query).toBe("139.130.4.5")
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and today`s weather.', (done) => {
+    jest.setTimeout(request.get('/current').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("Broome")
+      expect(ob.hasOwnProperty("weather")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and today`s weather. City provided', (done) => {
+    jest.setTimeout(request.get('/current?city=san miguel, buenos aires, argentina').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("San Miguel")
+      expect(ob.hasOwnProperty("weather")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and forecast', (done) => {
+    jest.setTimeout(request.get('/forecast').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("Broome")
+      expect(ob.hasOwnProperty("forecast")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and forecast. City provided', (done) => {
+    jest.setTimeout(request.get('/forecast?city=san miguel,buenos aires,argentina').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("San Miguel")
+      expect(ob.hasOwnProperty("forecast")).toBe(true)
+      done()
     }),5000)
   })
 })
@@ -29,10 +65,46 @@ describe('testing routes with mocked ip', () => {
     }),5000)
   })
 
-  it('Gets the an object with information about the location', () => {
+  it('Gets the object with information about the location', () => {
     jest.setTimeout(request.get('/location').set('X-Forwarded-For', '186.139.181.246').then(data => {
       const ob = JSON.parse(data.text)
       expect(ob.location.query).toBe("186.139.181.246")
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and today`s weather.', (done) => {
+    jest.setTimeout(request.get('/current').set('X-Forwarded-For', '186.139.181.246').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("Broome")
+      expect(ob.hasOwnProperty("weather")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the current location and today`s weather. City provided', (done) => {
+    jest.setTimeout(request.get('/current?city=san miguel, buenos aires, argentina').set('X-Forwarded-For', '186.139.181.246').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("San Miguel")
+      expect(ob.hasOwnProperty("weather")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and forecast', (done) => {
+    jest.setTimeout(request.get('/forecast').set('X-Forwarded-For', '186.139.181.246').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.query).toBe("186.139.181.246")
+      expect(ob.hasOwnProperty("forecast")).toBe(true)
+      done()
+    }),5000)
+  })
+
+  it('Gets the object with information about the location and forecast. City provided', (done) => {
+    jest.setTimeout(request.get('/forecast?city=san miguel,buenos aires,argentina').set('X-Forwarded-For', '186.139.181.246').then(data => {
+      const ob = JSON.parse(data.text)
+      expect(ob.location.city).toBe("San Miguel")
+      expect(ob.hasOwnProperty("forecast")).toBe(true)
+      done()
     }),5000)
   })
 })
